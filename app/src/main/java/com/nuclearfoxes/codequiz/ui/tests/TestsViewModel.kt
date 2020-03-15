dd66039797d8c15fa0ac1c948285e24fec0e4817
Zhekuson <zhekuson@gmail.com>
2020-03-15 13:04:20 +0300

    added material design support

--- app/src/main/java/com/nuclearfoxes/codequiz/ui/tests/TestsViewModel.kt
+++ app/src/main/java/com/nuclearfoxes/codequiz/ui/tests/TestsViewModel.kt
@@ -1,6 +1,15 @@
 package com.nuclearfoxes.codequiz.ui.tests
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 
 class TestsViewModel:ViewModel() {
+    /**
+     * list of chosen tags
+     */
+    lateinit var chosenTagsList: LiveData<ArrayList<String>>
+
+    lateinit var allAvailableTagsList: ArrayList<String>
+
+
 }
